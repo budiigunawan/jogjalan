389c606bc6c32427640ffdaff3841c8981fd7914
Budi Gunawan <budiindra.g@gmail.com>
2024-06-25 17:34:59 +0700

    feat: add get tag by id

--- src/routes/tag-route.ts
+++ src/routes/tag-route.ts
@@ -1,5 +1,6 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 import { tagService } from "../services";
+import { TagIdSchema } from "../schemas/tag-schema";
 
 const apiTags = ["Tag"];
 
@@ -30,5 +31,43 @@ tagRoute.openapi(
     }
 
     return c.json(tags, 200);
+  }
+);
+
+tagRoute.openapi(
+  {
+    method: "get",
+    path: "/{id}",
+    request: {
+      params: TagIdSchema,
+    },
+    description: "Get detail tag by id",
+    responses: {
+      200: {
+        description: "Tag details",
+      },
+      400: {
+        description: "There is no ID",
+      },
+      404: {
+        description: "Tag not found",
+      },
+    },
+    tags: apiTags,
+  },
+  async (c) => {
+    const id = c.req.param("id");
+
+    if (!id) {
+      return c.json({ message: "There is no ID" }, 401);
+    }
+
+    const tag = await tagService.getDetailById(id);
+
+    if (!tag) {
+      return c.json({ message: "Tag not found" }, 404);
+    }
+
+    return c.json(tag);
   }
 );
--- src/services/tag-service.ts
+++ src/services/tag-service.ts
@@ -4,3 +4,11 @@ export const getAll = async () => {
   const tags = await prisma.tag.findMany();
   return tags;
 };
+
+export const getDetailById = async (id: string) => {
+  const tag = await prisma.tag.findUnique({
+    where: { id },
+  });
+
+  return tag;
+};
