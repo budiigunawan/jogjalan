cdb9122e9c038a902b609228b682235414a870ee
Budi Gunawan <budiindra.g@gmail.com>
2024-07-03 07:49:29 +0700

    feat: add description to index.ts

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,15 @@ app.route("/categories", categoryRoute);
 app.route("/tags", tagRoute);
 
 // API DOCS
+const description =
+  "Jogjalan API for listing various tourism destinatios, restaurants, and todos in Yogyakarta.";
+
 app.doc31("/docs", {
   openapi: "3.0.0",
   info: {
     version: "1.0.0",
     title: "Jogjalan API",
-    description:
-      "Jogjalan API for listing various tourism destinatios, restaurants, and todos in Yogyakarta.",
+    description,
   },
 });
 
@@ -23,7 +25,7 @@ app.get("/swagger", swaggerUI({ url: "/docs" }));
 
 app.get("/", (c) => {
   return c.json({
-    message: "Jogjalan API",
+    message: description,
     swagger: "/swagger",
   });
 });
